032333b965ea912f88c9e873cfa2b4c95afe6763
Davi Dantas <90941164+Davidantasf@users.noreply.github.com>
2021-12-23 16:40:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int potencia = 2;
+            int numero = 5;
+            int r = 1;
+            for (int i = 1; i <= potencia; i++ )
+            {
+                r *= numero;
+            }
+            Console.WriteLine(r);
+
+            Console.ReadLine();
+        }
+    }
+}
